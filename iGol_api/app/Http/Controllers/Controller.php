b20d29dfa7963ef4c602268b3689db31378733b3
Josue Barrios <josue.barrios@unifyglobal.com>
2017-10-13 13:13:14 -0500

    Add responses and exceptions handler

--- iGol_api/app/Http/Controllers/Controller.php
+++ iGol_api/app/Http/Controllers/Controller.php
@@ -10,4 +10,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    //RESPONSES
+    public function createDataResponse($data){
+        return response()->json(['data' => $data], 200);
+    }
+
+    public function createSuccessResponse(){
+        return response()->json([], 204);
+    }
+
+    public function createErrorResponse($message,$code){
+        return response()->json(['message' => $message, 'code'=>$code], 400);
+    }
+
+    public function buildFailedValidationResponse(Request $request, array $errors) {
+        return response()->json(['errors' => $errors, 'code'=>config('customErrors.VALIDATION_ERROR')], 400);
+    }
+
 }
