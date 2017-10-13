5c618e7e6abaffe330132b5b2ebb18745e10fb79
Josue Barrios <josue.barrios@unifyglobal.com>
2017-10-13 15:05:56 -0500

    Add Venue

---
+++ iGol_api/app/Venue.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josue
+ * Date: 13/10/2017
+ * Time: 2:39 PM
+ */
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Venue extends Model {
+
+    protected $fillable = [
+        'name','address','phone','latitude','longitude','day_price','night_price','img','parking','play_area'
+    ];
+
+
+    protected $hidden = [
+
+    ];
+
+    public function tournament(){
+        return $this->belongsTo('App\Tournament');
+    }
+
+
+
+}
