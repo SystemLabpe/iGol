4804620f4e5c9142d35a8b5b1163c411ffd423a3
Josue Barrios <josue.barrios@unifyglobal.com>
2017-10-13 14:44:51 -0500

    Add user Controller

---
+++ iGol_api/app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: josue
+ * Date: 13/10/2017
+ * Time: 1:07 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+class UserController extends Controller{
+
+}
